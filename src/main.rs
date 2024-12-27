b30fe88172b2d280311367698e6b33c05257ca8d
Justin Lafferty <justin.lafferty@kw.com>
2024-12-26 21:54:18 -0500

    dump decoded file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 mod bencode;
-use bencode::bencode::decode;
 
-fn main() {
-    let value = decode(b"7:bencode");
-    println!("{:?}", value);
+use bencode::bencode::decode;
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err("Usage: ./bencode <torrent_name>".to_string());
+    }
+
+    let torrent_name = &args[1];
+    let file_contents = fs::read(torrent_name).expect("Couldn't read torrent file");
+    let decoded_file = decode(&file_contents).unwrap();
+
+    println!("{:?}", decoded_file);
+
+    Ok(())
 }
