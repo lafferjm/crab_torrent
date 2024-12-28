fb0a05103e1f26824cf2b4d9bf5b2593b5498441
Justin Lafferty <justin.lafferty@kw.com>
2024-12-28 11:42:02 -0500

    add torrent module tests

--- src/torrent.rs
+++ src/torrent.rs
@@ -2,7 +2,7 @@ use crate::bencode::Bencode;
 use chrono::{DateTime, Utc};
 use std::collections::BTreeMap;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Torrent {
     pub announce: String,
     pub created_by: String,
@@ -36,4 +36,36 @@ fn get_string(dictionary: &BTreeMap<Vec<u8>, Bencode>, key: &[u8]) -> Option<Str
         .get(key)
         .and_then(|value| value.as_string())
         .map(|value| value.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Torrent;
+    use crate::bencode::Bencode;
+    use chrono::DateTime;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn it_converts_bencode_to_torrent() {
+        let mut bencode_dictionary: BTreeMap<Vec<u8>, Bencode> = BTreeMap::new();
+        bencode_dictionary.insert(
+            b"announce".to_vec(),
+            Bencode::String(b"http://domain/announce".to_vec()),
+        );
+        bencode_dictionary.insert(
+            b"created by".to_vec(),
+            Bencode::String(b"created by me".to_vec()),
+        );
+        bencode_dictionary.insert(b"creation date".to_vec(), Bencode::Integer(1735403744163));
+        let bencode_dictionary = Bencode::Dictionary(bencode_dictionary);
+
+        let result = Torrent::from_bencode(&bencode_dictionary).unwrap();
+        let expected = Torrent {
+            announce: "http://domain/announce".to_string(),
+            created_by: "created by me".to_string(),
+            creation_date: DateTime::from_timestamp(1735403744163, 0).unwrap(),
+        };
+
+        assert_eq!(result, expected);
+    }
 }
